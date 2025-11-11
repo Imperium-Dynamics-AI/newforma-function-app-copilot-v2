c51d04757166fde0be13fe5815f52c0e818738b5
Ahmed Farrukh <ahmed.farrukh@imperiumdynamics.com>
2025-11-11 18:54:32 +0500

    Added separate file for routes and used that in function_app.py file

--- function_app/function_app.py
+++ function_app/function_app.py
@@ -1,17 +1,9 @@
+# function_app.py
+
 import azure.functions as func
-from src.handlers.todo_handler import TodoHandler
-import json
+from src.routes.todo_routes import register_todo_routes
 
 app = func.FunctionApp()
-todo_handler = TodoHandler()
 
-
-@app.function_name(name="create_todo")
-@app.route(route="create_todo", methods=["POST"])
-async def create_todo(req: func.HttpRequest) -> func.HttpResponse:
-    response, status_code = await todo_handler.handle_create_todo_request(req)
-    return func.HttpResponse(
-        json.dumps(response),
-        status_code=status_code,
-        mimetype="application/json"
-    )
+# Register all routes
+register_todo_routes(app)
