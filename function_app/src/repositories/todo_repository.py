0a6f0919de5ec5f1a5a21580d529a34f02c94d54
Ayan Bin Rizwan <ayanbin.rizwan@imperiumdynamics.com>
2025-11-11 20:13:10 +0500

    Added dynamic url builder in url_builder.py file

--- function_app/src/repositories/todo_repository.py
+++ function_app/src/repositories/todo_repository.py
@@ -1,14 +1,15 @@
 from src.repositories.graph_client import GraphClient
-
+from src.utils.url_builder import UrlBuilder
 class TodoRepository:
     def __init__(self):
         self.graph_client = GraphClient()
+        self.url_builder = UrlBuilder()
 
     async def create_list(self, user_email: str, list_name: str):
-        endpoint = f"/users/{user_email}/todo/lists"
+        endpoint = self.url_builder.create_list_url(user_email)
         payload = {"displayName": list_name}
         return await self.graph_client.request("POST", endpoint, json_body=payload)
     
     async def fetch_lists(self, user_email: str):
-        endpoint = f"/users/{user_email}/todo/lists"
+        endpoint = self.url_builder.fetch_lists_url(user_email)
         return await self.graph_client.request("GET", endpoint)
